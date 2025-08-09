03d8c72906135d19357d24f0fa303b7c6a3e9221
Jeet <jeetpatil3333@gmail.com>
2025-08-09 20:18:50 +0530

    fixed minnor errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -257,15 +257,26 @@ const changeCurrentPassword = asyncHandler( async (req, res) => {
 
     return res
     .status(200)
-    .json(new ApiResponse(200, {}, "Password changed successfully"))
+    .json(
+        new ApiResponse(
+            200, 
+            {}, 
+            "Password changed successfully"
+        )
+    )
     
-
 })
 
 const getCurrentUser = asyncHandler( async (req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(
+        new ApiResponse(
+            200, 
+            req.user, 
+            "User fetched successfully"
+        )
+    )
 })
 
 const updateAccountDetails = asyncHandler( async (req, res) => {
@@ -276,7 +287,7 @@ const updateAccountDetails = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id, 
         {
             $set: {
@@ -299,6 +310,8 @@ const updateUserAvatar = asyncHandler( async (req, res) => {
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is missing")
     }
+
+    // TODO: delete old image from cloudinary
     
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     
